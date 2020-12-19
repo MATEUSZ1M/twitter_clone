c181fa2f575cdc8b71b29febde38164b13273b33
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-12-19 21:00:39 +0100

    Create Sidebar component

--- twitter_clone/src/App.js
+++ twitter_clone/src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>twitter clone</h1>
+      {/* sidebar */}
+      {/* feed */}
+      {/* widgets */}
     </div>
   );
 }
